ef1c36908b8abbf9715451f20c7f4042d3e0df73
Loïc Delanoë <loic.del4127@gmail.com>
2023-10-04 14:46:13 +0200

    Finished exercice

--- OCR-starter/chapitre2_exercice2.js
+++ OCR-starter/chapitre2_exercice2.js
@@ -1,3 +1,6 @@
 // Celsius - Fahrenheit
 
+let celsius = prompt('Insérer une température en Celsius : ');
+let fahrenheit = (celsius * 9/5) + 32;
 
+alert(`La température en Fahrenheit est de ${fahrenheit}`);

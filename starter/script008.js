50b7542f46c9380b0b6c9a60648c109ae36e114f
Loïc Delanoë <loic.del4127@gmail.com>
2023-10-04 14:20:19 +0200

    Finished exercice

--- starter/script008.js
+++ starter/script008.js
@@ -3,19 +3,21 @@
 */
 
 // 1. Initialiser une variable firstName à John et afficher sa valeur dans la console
-
+const firstName = 'John';
+console.log(firstName);
 
 // 2. Initialiser une variable lastName à Smith et une variable age à 28
-
+const lastName = 'Smith';
+let age = 28;
 
 // 3. Initialiser une variable fullAge à true et l'afficher
-
+const fullAge = true;
 
 // 4. Déclarer une variable job et afficher sa valeur dans la console
-
-
+let job = '';
+console.log(job);
 // 5. Affecter la valeur Enseignat à la variable job et l'afficher 
-
+job = 'Enseigant';
 
 // 6. Règles de nommage des variables : le nom ne peut pas 
 // - commencer par un chiffre

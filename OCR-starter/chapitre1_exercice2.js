0b42b455595d1656ebec9d86aad0da8d0d7bce2e
Loïc Delanoë <loic.del4127@gmail.com>
2023-10-04 14:23:38 +0200

    Finished exercice

--- OCR-starter/chapitre1_exercice2.js
+++ OCR-starter/chapitre1_exercice2.js
@@ -8,4 +8,10 @@ Affichez dans la console les opérations de base avec les nombres 6 et 3 :
 - leur modulo
 Avant d'exécuter le code, devinez à chaque fois le résultat de l'opération
 */
+let somme = 6 + 3;
+let difference = 6 - 3;
+let multiplication = 6 * 3;
+let division = 6 / 3;
+let modulo = 6 % 3;
 
+console.log(somme, difference, multiplication, division, modulo);
